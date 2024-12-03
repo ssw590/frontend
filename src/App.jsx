6fd2745034dd3a913b8495fc69b44508dd7b137b
Andrew Yurovchak <andy@yurovchak.net>
2024-12-03 12:38:32 -0500

    add functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,56 @@
-import { useRef } from 'react'
+import { useRef, useState } from 'react'
+
+function SearchItem(props) {
+  console.log(props)
+
+  return (
+    <div style={{display: "flex", flexDirection: "row", justifyContent: "space-between", width:"100%"}}>
+      <p>{props.result.name}</p>
+      <p>{props.result.price}</p>
+    </div>
+  )
+}
 
 function App() {
-  const textRef = useRef()
-  const respRef = useRef()
+  const searchRef = useRef()
+  const displayRef = useRef()
+  const [results, setResults] = useState([])
 
-  const handleSubmit = async (e) => {
-    alert(`submitted ${textRef.current.value}`)
+  const backendUrl = import.meta.env.VITE_BACKEND_URL
 
-    // fetch("") will fill in once deployed
+  const handleSearch = async (e) => {
+
+    e.preventDefault()
+
+    const queryString = new URLSearchParams({
+      search: searchRef.current.value
+    })
+
+    const response = await fetch(backendUrl + '?' + queryString.toString())
+
+    const items = await response.json()
+
+    console.log(items)
+
+    // XSS
+    displayRef.current.innerHTML = "Search results for: " + searchRef.current.value
+    
+    setResults(items)
   }
 
   return (
     <>
-      <h1>What is your name?</h1>
-      <form onSubmit={handleSubmit}>
-        <label>Name:
-          <input type='text' ref={textRef}></input>
-        </label>
-        <button type='submit'>Submit</button>
+      <h1>Online market place</h1>
+      <form onSubmit={handleSearch}>
+        <input type='text' placeholder='search for a product...' ref={searchRef}></input>
+        <button type='submit'>Search</button>
       </form>
-      <h1 ref={respRef}></h1>
+      <div>
+        <h2 ref={displayRef}></h2>
+        {results.map((result) => {
+          return <SearchItem result={result}></SearchItem>
+        })}
+      </div>
     </>
   )
 }
